509683da15e450949ce8bf8d37154d107cd60806
jamesjsanders <james.sanders@15five.com>
2019-11-04 18:12:13 -0800

    commented out role switch

--- provider.tf
+++ provider.tf
@@ -2,9 +2,9 @@ provider "aws" {
   profile = local.aws_profile
   region  = local.aws_region
 
-  assume_role {
-    role_arn = local.aws_role_arn
-  }
+  #  assume_role {
+  #    role_arn = local.aws_role_arn
+  #  }
 }
 
 data "aws_caller_identity" "current" {
